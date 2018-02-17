f0e6f4d9323ff9a37df7133727cb96c59dda613d
Zohar Babin <z.babin@gmail.com>
2018-02-17 22:33:27 +0200

    cleanup of client and ks generation in editor-services.php

--- reference-samples/php/editor-services.php
+++ reference-samples/php/editor-services.php
@@ -4,16 +4,19 @@ require_once('configs.php');
 require_once('./kaltura-php5/KalturaClient.php');
 
 /**
-* Helper function to generate a Kaltura session given privileges and userId
-* @param string    $privileges     The privileges to set on this session
-* @param string    $userId         The ID of the user to set on this session
+* Helper function to create a Kaltura Client and generate a Kaltura session given privileges, userId and type
+* @param string                $privileges     The privileges to set on this session
+* @param string                $userId         The ID of the user to set on this session
+* @param KalturaSessionType    $type           Whether to create ADMIN or USER session
+* @return KalturaClient        A session initialized Kaltura Client
 **/
-function createSession($privileges, $userId) {
+function getKalturaClient($privileges, $userId, $type) {
     $config = new KalturaConfiguration();
     $config->serviceUrl = KALTURA_SERVICE_URL;
     $client = new KalturaClient($config);
-    $ks = $client->generateSessionV2(KALTURA_ADMIN_SECRET, $userId, KalturaSessionType::USER, KALTURA_PARTNER_ID, 86400, $privileges);
-    return $ks;
+    $ks = $client->generateSessionV2(KALTURA_ADMIN_SECRET, $userId, $type, KALTURA_PARTNER_ID, 86400, $privileges);
+    $client->setKs($ks);
+    return $client;
 }
 
 /**
@@ -26,7 +29,8 @@ function getEditKS($entryId) {
         $privileges  = 'sview:'.$entryId;
     else // This user can edit this entry (Trim this entry) and create new entries (create clips or quiz):
         $privileges  = 'sview:'.$entryId.',edit:'.$entryId;
-    return createSession($privileges, TEST_USER_ID);
+    $kclient = getKalturaClient($privileges, TEST_USER_ID, KalturaSessionType::USER);
+    return $kclient->getKs();
 }
 
 /**
@@ -37,7 +41,8 @@ function getEditKS($entryId) {
 function getPreviewKS($entryId) {
     // We add "setrole:PLAYBACK_BASE_ROLE" privilege so that this session will not be allowed to perform any action other than the list of actions needed by the player to play this entry.
     $privileges  = 'disableentitlementforentry:'.$entryId.',setrole:PLAYBACK_BASE_ROLE,sview:' . $entryId;
-    return createSession($privileges, '');
+    $kclient = getKalturaClient($privileges, '', KalturaSessionType::USER);
+    return $kclient->getKs();
 }
 
 /**
@@ -47,12 +52,12 @@ function getPreviewKS($entryId) {
 **/
 function getDisplayNameForUserId ($userId) {
     // Example using the Kaltura user service (if your managing your users outside of Kaltura, this is where you call your own user management service to get the display name for your userId):
-    $config = new KalturaConfiguration();
-    $config->serviceUrl = KALTURA_SERVICE_URL;
-    $client = new KalturaClient($config);
-    $ks = $client->generateSessionV2(KALTURA_ADMIN_SECRET, 'kedit-get-user-name', KalturaSessionType::ADMIN, KALTURA_PARTNER_ID, 86400, '');
-    $client->setKs($ks);
-    $user = $client->user->get($userId); 
+    $kclient = getKalturaClient('', 'kedit-get-user-name', KalturaSessionType::ADMIN);
+    try {
+        $user = $kclient->user->get($userId); 
+    }catch(Exception $err){
+        //userId doesn't exist in the system
+    }
     $userDisplayName = $userId; //in case we don't have a display name available for this user, use the userId
     if (isset($user->fullName) && $user->fullName != '' && $user->fullName != $userId)
         $userDisplayName = $user->fullName . '(' . $userId . ')';
